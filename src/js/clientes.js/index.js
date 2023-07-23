b826c3e1c7abf98e640f6502b5b570157ea224fc
Livni <llmc0383@gmail.com>
2023-07-23 12:42:56 -0600

    se agregaron las constantes al index

---
+++ src/js/clientes.js/index.js
@@ -0,0 +1,5 @@
+const formulario = document.querySelector('form')
+const botonBuscar = document.getElementById('botonBuscar');
+const botonModificar = document.getElementById('botonModificar');
+const botonGuardar = document.getElementById('botonGuardar');
+const botonCancelar = document.getElementById('botonCancelar');
