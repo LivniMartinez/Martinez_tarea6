97c5f8b90e00f344b29e3184fe52ab6f46345410
Livni <llmc0383@gmail.com>
2023-07-23 12:13:56 -0600

    se creo el buscar para la vista de clientes

--- vistas/clientes/buscar.php
+++ vistas/clientes/buscar.php
@@ -1,13 +1,13 @@
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Formulario de ingreso de clientes</h1>
+        <h1 class="text-center">Buscar clientes</h1>
         <div class="row justify-content-center">
-            <form action="/Martinez_tarea6/controladores/clientes/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <form action="/Martinez_tarea6/controladores/clientes/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                    <label for="cliente_nombre">Nombre del cliente</label>
-                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control">
+                        <label for="cliente_nombre">Nombre del cliente</label>
+                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
@@ -18,10 +18,10 @@
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                        <button type="submit" class="btn btn-info w-100">Buscar</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
-<?php include_once '../../includes/footer.php'?>
+    <?php include_once '../../includes/footer.php'?>
