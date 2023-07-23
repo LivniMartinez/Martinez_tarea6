b679b9a498b379e0c3c56c194976517285260db3
Livni <llmc0383@gmail.com>
2023-07-23 12:46:11 -0600

    se agrego el evento guardar

--- src/js/clientes.js/index.js
+++ src/js/clientes.js/index.js
@@ -3,3 +3,13 @@ const botonBuscar = document.getElementById('botonBuscar');
 const botonModificar = document.getElementById('botonModificar');
 const botonGuardar = document.getElementById('botonGuardar');
 const botonCancelar = document.getElementById('botonCancelar');
+
+
+
+const guardar = async (evento) => {
+    evento.preventDefault();
+    if(!validarFormulario(formulario, ['cliente_id'])){
+        alert('Debe llenar todos los campos');
+        return 
+    }
+}
