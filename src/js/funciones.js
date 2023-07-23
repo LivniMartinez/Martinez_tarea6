1b0a9c89b76a5e4c9b0d45df448d565e668312d6
Livni <llmc0383@gmail.com>
2023-07-23 13:14:41 -0600

    se creo la validacion del formulario

---
+++ src/js/funciones.js
@@ -0,0 +1,19 @@
+const validarFormulario = (formulario, excepciones = [] ) => {
+    const elements = formulario.querySelectorAll("input, select, textarea");
+    let validarFormulario = []
+    elements.forEach( element => {
+        if(!element.value.trim() && ! excepciones.includes(element.id) ){
+            element.classList.add('is-invalid');
+          
+            validarFormulario.push(false)
+        }else{
+            element.classList.remove('is-invalid');
+        }
+    });
+
+    let noenviar = validarFormulario.includes(false);
+
+    return !noenviar;
+}
+
+        
